2c9ed232f39d0b66294097d4f87d32efeed9b047
Taravann Heng <heng.t.taravann@gmail.com>
2022-10-01 01:52:52 +0700

    feat: add author and description meta tag to head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,9 @@ function MyApp({ Component, pageProps }: AppProps) {
   return <Fragment>
     <Head>
       <title>Taravann | Portfolio</title>
-      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="author" content="Taravann Heng" />
+      <meta name="description" content="This is my personal portfolio site featuring some of the projects that I've worked on."  />
     </Head>
     <Component {...pageProps} />
   </Fragment>
