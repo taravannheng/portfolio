f060ca8305f3690af736f0d032947f5e72c02160
Taravann Heng <heng.t.taravann@gmail.com>
2022-10-01 01:48:22 +0700

    feat: add title and viewport meta tag to head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
+import { Fragment } from 'react'
+import Head from 'next/head'
+
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Fragment>
+    <Head>
+      <title>Taravann | Portfolio</title>
+      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+    </Head>
+    <Component {...pageProps} />
+  </Fragment>
 }
 
 export default MyApp
