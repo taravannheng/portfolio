469988357e663ad136a974a519ee255a82748c55
Taravann Heng <heng.t.taravann@gmail.com>
2022-10-02 18:46:23 +0700

    feat: add favicons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,12 @@ function MyApp({ Component, pageProps }: AppProps) {
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta name="author" content="Taravann Heng" />
       <meta name="description" content="This is my personal portfolio site featuring some of the projects that I've worked on."  />
+      <link rel="icon" type="image/png" sizes="512x512" href="/images/favicons/favicon-512.png" />
+      <link rel="icon" type="image/png" sizes="192x192" href="/images/favicons/favicon-192.png" />
+      <link rel="apple-touch-icon-precomposed" type="image/png" sizes="144x144" href="/images/favicons/favicon-144.png" />
+      <link rel="shortcut icon" sizes='96x96' href="/images/favicons/favicon-96.png" type="image/png"/>
+      <link rel="shortcut icon" sizes="32x32" href="/images/favicons/favicon-32.png" type="image/png"/>
+      <link rel="shortcut icon" sizes="16x16" href="/images/favicons/favicon-16.png" type="image/png"/>
     </Head>
     {/* <!-- Google tag (gtag.js) --> */}
     <Script strategy='lazyOnload' async src={`"https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID}"`}></Script>
