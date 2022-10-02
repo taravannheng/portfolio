fcf78ef3ef3d19adfee48bd261f8eec1df602a09
Taravann Heng <heng.t.taravann@gmail.com>
2022-10-02 17:55:35 +0700

    feat: add illustration to work section

--- components/work/work.tsx
+++ components/work/work.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 import classes from './work.module.scss';
 import WorkCard from './work-card';
 
@@ -7,12 +9,17 @@ const WorkSection = () => {
   return (
     <div className={classes.work}>
       <h2 className={classes.work__title}>Work Experience</h2>
-      <div className={classes.work__list}>
-        <div className={classes.list__left_section}>
-          <WorkCard company={workExperience[0].company} logo={workExperience[0].logo} positions={workExperience[0].positions} />
+      <div className={classes.content}>
+        <div className={classes.content__list}>
+          <div className={classes.list__left_section}>
+            <WorkCard company={workExperience[0].company} logo={workExperience[0].logo} positions={workExperience[0].positions} />
+          </div>
+          <div className={classes.list__right_section}>
+            <WorkCard company={workExperience[1].company}  logo={workExperience[1].logo} positions={workExperience[1].positions} />
+          </div>
         </div>
-        <div className={classes.list__right_section}>
-          <WorkCard company={workExperience[1].company}  logo={workExperience[1].logo} positions={workExperience[1].positions} />
+        <div className={classes.content__illustration}>
+          <Image layout='responsive' src='/images/illustrations/illustration-work.png' width={800} height={800} alt="illustration of work" />
         </div>
       </div>
     </div>
