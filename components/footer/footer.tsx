c292e1c6d865dfd5af58e597bc53585c3b164ea2
Taravann Heng <heng.t.taravann@gmail.com>
2022-10-02 10:44:27 +0700

    feat: add footer section

---
+++ components/footer/footer.tsx
@@ -0,0 +1,53 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import classes from './footer.module.scss';
+
+const getYear = () => {
+  return new Date().getFullYear().toString();
+};
+
+const FooterSection = () => {
+  return (
+    <div className={classes.footer}>
+      <div className={classes.footer__social}>
+        <div className={classes.social__icon}>
+          <Link
+            href="https://www.linkedin.com/in/taravann-heng"
+          >
+            <a target="_blank">
+              <Image
+                src="/images/social/linkedin.svg"
+                width="36"
+                height="36"
+                alt="LinkedIn"
+              />
+            </a>
+          </Link>
+        </div>
+        <div className={classes.social__icon}>
+          <Link href="https://github.com/taravannheng">
+            <a target="_blank">
+              <Image
+                src="/images/social/github.svg"
+                width="36"
+                height="36"
+                alt="GitHub"
+              />
+            </a>
+          </Link>
+        </div>
+      </div>
+      <div className={classes.footer__copyright}>
+        <p className={classes.copyright__credit}>
+          Design and Built by Taravann Heng
+        </p>
+        <p className={classes.copyright__text}>
+          All Rights Reserved · {getYear()}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default FooterSection;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import HeroSection from '../components/hero/hero';
 import WorkSection from '../components/work/work';
+import FooterSection from '../components/footer/footer';
 
 const HomePage = () => {
   return (
     <div>
       <HeroSection />
       <WorkSection />
+      <FooterSection />
     </div>
   );
 };
