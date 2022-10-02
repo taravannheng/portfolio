88fb8d956d6e02d42af525713eb3c0f31dc4f566
Taravann Heng <heng.t.taravann@gmail.com>
2022-10-02 22:35:00 +0700

    feat: update title in hero section

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -8,7 +8,7 @@ const HeroSection = () => {
     <div className={classes.hero}>
       <Profile src="/images/profile/profile.jpg" />
       <h1 className={classes.hero__title}>Taravann</h1>
-      <h2 className={classes.hero__subtitle}><span className={classes.subtitle__optional}>A</span> Web Developer<span className={classes.subtitle__optional}> · Previously A UX Designer</span></h2>
+      <h2 className={classes.hero__subtitle}><span className={classes.subtitle__optional}>A</span> Frontend Developer<span className={classes.subtitle__optional}> · Previously A UX Designer</span></h2>
       <p className={classes.hero__bio}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis. Ut commodo efficitur neque. Ut diam quam, semper iaculis condimentum ac, vestibulum eu nisl.</p>
     </div>
   )
