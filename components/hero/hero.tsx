fa8bec5a88d3bca112311327e10c336d8b607c94
Taravann Heng <heng.t.taravann@gmail.com>
2022-10-04 22:04:39 +0700

    feat: add illusttration to hero section

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -10,6 +10,9 @@ const HeroSection = () => {
       <h1 className={classes.hero__title}>Taravann</h1>
       <h2 className={classes.hero__subtitle}><span className={classes.subtitle__optional}>A</span> Frontend Developer<span className={classes.subtitle__optional}> · Previously A UX Designer</span></h2>
       <p className={classes.hero__bio}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis. Ut commodo efficitur neque. Ut diam quam, semper iaculis condimentum ac, vestibulum eu nisl.</p>
+      <div className={classes.hero__illustration}>
+        <Image src='/images/illustrations/illustration-hero.png' width={800} height={800} alt="illustration" />
+      </div>
     </div>
   )
 }
