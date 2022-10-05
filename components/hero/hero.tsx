2075fee977f912b20bc98838d331af14498fe8ac
Taravann Heng <heng.t.taravann@gmail.com>
2022-10-05 23:46:42 +0700

    feat: add bio description

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -9,7 +9,7 @@ const HeroSection = () => {
       <Profile src="/images/profile/profile.jpg" />
       <h1 className={classes.hero__title}>Taravann</h1>
       <h2 className={classes.hero__subtitle}><span className={classes.subtitle__optional}>A</span> Frontend Developer<span className={classes.subtitle__optional}> · Previously A UX Designer</span></h2>
-      <p className={classes.hero__bio}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis. Ut commodo efficitur neque. Ut diam quam, semper iaculis condimentum ac, vestibulum eu nisl.</p>
+      <p className={classes.hero__bio}>Hello there! I&apos;m Taravann Heng. I have some experiences doing both design and development work for mobile apps and websites. Regarding education, I have a Bachelor&apos;s degree in International Banking and Finance. Also I have a Master&apos;s degree in Information Technology. Currently, I want to work and focus on frontend web development.</p>
       <div className={classes.hero__illustration}>
         <Image src='/images/illustrations/illustration-hero.png' width={800} height={800} alt="illustration" />
       </div>
