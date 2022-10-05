ee1fb8ed9971eea509e97d51e2e30581d332d7dc
Taravann Heng <heng.t.taravann@gmail.com>
2022-10-06 02:07:11 +0700

    feat: add contact section

---
+++ components/contact/contact.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+
+import classes from './contact.module.scss';
+
+const ContactSection = () => {
+  return (
+    <div className={classes.contact}>
+      <h2 className={classes.contact__title}>Contact</h2>
+      <div className={`${classes.contact__content} ${classes.content}` }>
+        <div className={classes.content__illustration}>
+          <Image layout='responsive' src='/images/illustrations/illustration-contact.png' width={800} height={800} alt="illustration of contact" />
+        </div>
+        <div className={classes.content__form_container}>
+          <form
+            action="https://formsubmit.co/heng.p.taravann@gmail.com"
+            method="POST"
+            className={classes.form}
+          >
+            <input type="text" name="name" placeholder="Name" required />
+            <input type="email" name="email" placeholder="Email" required />
+            <textarea placeholder="Enter your message here..."></textarea>
+            <div className={classes.form__submit_button_container}>
+              <button type="submit">SEND</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactSection;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import HeroSection from '../components/hero/hero';
 import SkillsSection from '../components/skills/skills';
 import WorkSection from '../components/work/work';
+import ContactSection from '../components/contact/contact';
 import FooterSection from '../components/footer/footer';
 
 const HomePage = () => {
@@ -9,6 +10,7 @@ const HomePage = () => {
       <HeroSection />
       <SkillsSection />
       <WorkSection />
+      <ContactSection />
       <FooterSection />
     </div>
   );
