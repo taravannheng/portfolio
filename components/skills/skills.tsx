442bc85c881ccd3f3e417891fdcfac6f70d65a7a
Taravann Heng <heng.t.taravann@gmail.com>
2022-10-07 22:32:21 +0700

    refactor: update technology list

--- components/skills/skills.tsx
+++ components/skills/skills.tsx
@@ -14,22 +14,22 @@ const SkillsSection = () => {
           <p>Some technologies I&apos;m familiar with:</p>
           <div className={classes.technology__container}>
             <ul>
-              <li><Image src="/images/languages/aws.svg" width={36} height={36} alt="amazon web services" /></li>
-              <li><Image src="/images/languages/babel.svg" width={36} height={36} alt="babel" /></li>
-              <li><Image src="/images/languages/bootstrap.svg" width={36} height={36} alt="bootstrap" /></li>
-              <li><Image src="/images/languages/eslint.svg" width={36} height={36} alt="eslint" /></li>
+              {/* <li><Image src="/images/languages/aws.svg" width={36} height={36} alt="amazon web services" /></li> */}
+              {/* <li><Image src="/images/languages/babel.svg" width={36} height={36} alt="babel" /></li> */}
+              {/* <li><Image src="/images/languages/bootstrap.svg" width={36} height={36} alt="bootstrap" /></li> */}
+              {/* <li><Image src="/images/languages/eslint.svg" width={36} height={36} alt="eslint" /></li> */}
               <li><Image src="/images/languages/firebase.svg" width={36} height={36} alt="firebase" /></li>
-              <li><Image src="/images/languages/graphql.svg" width={36} height={36} alt="graphql" /></li>
+              {/* <li><Image src="/images/languages/graphql.svg" width={36} height={36} alt="graphql" /></li> */}
               <li><Image src="/images/languages/javascript.svg" width={36} height={36} alt="javascript" /></li>
-              <li><Image src="/images/languages/materialui.svg" width={36} height={36} alt="material ui" /></li>
+              {/* <li><Image src="/images/languages/materialui.svg" width={36} height={36} alt="material ui" /></li> */}
               <li><Image src="/images/languages/nextjs.svg" width={36} height={36} alt="next js" /></li>
               <li><Image src="/images/languages/python.svg" width={36} height={36} alt="python" /></li>
               <li><Image src="/images/languages/react.svg" width={36} height={36} alt="react" /></li>
               <li><Image src="/images/languages/redux.svg" width={36} height={36} alt="redux" /></li>
               <li><Image src="/images/languages/sass.svg" width={36} height={36} alt="sass" /></li>
-              <li><Image src="/images/languages/tailwind.svg" width={36} height={36} alt="tailwind" /></li>
+              {/* <li><Image src="/images/languages/tailwind.svg" width={36} height={36} alt="tailwind" /></li> */}
               <li><Image src="/images/languages/typescript.svg" width={36} height={36} alt="typescript" /></li>
-              <li><Image src="/images/languages/webpack.svg" width={36} height={36} alt="webpack" /></li>
+              {/* <li><Image src="/images/languages/webpack.svg" width={36} height={36} alt="webpack" /></li> */}
             </ul>
           </div>
         </div>
