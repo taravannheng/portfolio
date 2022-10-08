dd14ce55a50af2ce21f1a9bd306c32e67032d9be
Taravann Heng <heng.t.taravann@gmail.com>
2022-10-08 19:10:35 +0700

    refactor: add loading state for initial page load

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+
 import HeroSection from '../components/hero/hero';
 import SkillsSection from '../components/skills/skills';
 import WorkSection from '../components/work/work';
@@ -6,15 +8,28 @@ import FooterSection from '../components/footer/footer';
 import Loader from '../components/loader/loader';
 
 const HomePage = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 6000);
+  }, []);
+
   return (
-    <div>
-      <Loader />
-      <HeroSection />
-      <SkillsSection />
-      <WorkSection />
-      <ContactSection />
-      <FooterSection />
-    </div>
+    <>
+      {loading && <Loader />}
+
+      {!loading && (
+        <div>
+          <HeroSection />
+          <SkillsSection />
+          <WorkSection />
+          <ContactSection />
+          <FooterSection />
+        </div>
+      )}
+    </>
   );
 };
 
