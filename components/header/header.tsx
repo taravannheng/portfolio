621bf4a1913a3575046ed502be51e446e44d3ce3
Taravann Heng <heng.t.taravann@gmail.com>
2022-10-09 15:57:48 +0700

    refactor: add condition for hiding nav

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -6,7 +6,7 @@ import classes from './header.module.scss';
 const Header = ({ hideElement }) => {
   return (
     <header className={classes.header}>
-      <div className={classes.header__content}>
+      <div className={classes.header__content} style={{justifyContent: hideElement === 'nav' ? 'center' : 'justify-between'}}>
         <div className={classes.header__logo}>
           <Link href="/">
             <Image
@@ -18,38 +18,40 @@ const Header = ({ hideElement }) => {
             />
           </Link>
         </div>
-        <nav className={classes.header__nav}>
-          {hideElement !== 'home' && (
-            <a className={`${classes.nav} ${classes.nav__link}`}>
-              <Link href="/">Home</Link>
-            </a>
-          )}
-          {hideElement !== 'skills' && (
-            <a className={`${classes.nav} ${classes.nav__link}`}>
-              <Link href="#skills">Skills</Link>
-            </a>
-          )}
-          {hideElement !== 'works' && (
-            <a className={`${classes.nav} ${classes.nav__link}`}>
-              <Link href="#works">Works</Link>
-            </a>
-          )}
-          {hideElement !== 'projects' && (
-            <a className={`${classes.nav} ${classes.nav__link}`}>
-              <Link href="#projects">Projects</Link>
-            </a>
-          )}
-          {hideElement !== 'contact' && (
-            <a className={`${classes.nav} ${classes.nav__link}`}>
-              <Link href="#contact">Contact</Link>
-            </a>
-          )}
-          {hideElement !== 'credits' && (
-            <a className={`${classes.nav} ${classes.nav__link}`}>
-              <Link href="/credits">Credits</Link>
-            </a>
-          )}
-        </nav>
+        {hideElement !== 'nav' && (
+          <nav className={classes.header__nav}>
+            {hideElement !== 'home' && (
+              <a className={`${classes.nav} ${classes.nav__link}`}>
+                <Link href="/">Home</Link>
+              </a>
+            )}
+            {hideElement !== 'skills' && (
+              <a className={`${classes.nav} ${classes.nav__link}`}>
+                <Link href="#skills">Skills</Link>
+              </a>
+            )}
+            {hideElement !== 'works' && (
+              <a className={`${classes.nav} ${classes.nav__link}`}>
+                <Link href="#works">Works</Link>
+              </a>
+            )}
+            {hideElement !== 'projects' && (
+              <a className={`${classes.nav} ${classes.nav__link}`}>
+                <Link href="#projects">Projects</Link>
+              </a>
+            )}
+            {hideElement !== 'contact' && (
+              <a className={`${classes.nav} ${classes.nav__link}`}>
+                <Link href="#contact">Contact</Link>
+              </a>
+            )}
+            {hideElement !== 'credits' && (
+              <a className={`${classes.nav} ${classes.nav__link}`}>
+                <Link href="/credits">Credits</Link>
+              </a>
+            )}
+          </nav>
+        )}
       </div>
     </header>
   );
