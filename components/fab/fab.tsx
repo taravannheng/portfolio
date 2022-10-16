383e9c057a62eb5316756f3e833820181bb58e03
Taravann Heng <heng.t.taravann@gmail.com>
2022-10-16 09:50:39 +0700

    feat: add button for downloading cv

---
+++ components/fab/fab.tsx
@@ -0,0 +1,9 @@
+import classes from './fab.module.scss';
+
+const Fab = () => {
+  return (
+    
+  );
+};
+
+export default Fab;
--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -1,20 +1,39 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
-import classes from './hero.module.scss'
-import Profile from '../profile/profile'
+import classes from './hero.module.scss';
+import Profile from '../profile/profile';
+import Fab from '../fab/fab';
 
 const HeroSection = () => {
   return (
     <section className={classes.hero}>
       <Profile src="/images/profile/profile.jpg" />
       <h1 className={classes.hero__title}>Taravann</h1>
-      <h2 className={classes.hero__subtitle}><span className={classes.subtitle__optional}>A</span> Frontend Developer<span className={classes.subtitle__optional}> · Previously A UX Designer</span></h2>
-      <p className={classes.hero__bio}>Hello, there! I&apos;m Taravann Heng. A developer/designer based in Cambodia. I have some experiences doing both UX design and frontend development works for mobile apps and websites. Currently, I&apos;m focusing on frontend web development.</p>
+      <h2 className={classes.hero__subtitle}>
+        <span className={classes.subtitle__optional}>A</span> Frontend Developer
+        <span className={classes.subtitle__optional}>
+          {' '}
+          · Previously A UX Designer
+        </span>
+      </h2>
+      <p className={classes.hero__bio}>
+        Hello, there! I&apos;m Taravann Heng. A developer/designer based in
+        Cambodia. I have some experiences doing both UX design and frontend
+        development works for mobile apps and websites. Currently, I&apos;m
+        focusing on frontend web development.
+      </p>
+      <a href="/cv/taravann-heng-cv.pdf" download="taravann-heng-cv" className={classes.hero__download_cv_button}>Download CV</a>
       <div className={classes.hero__illustration}>
-        <Image src='/images/illustrations/illustration-hero.png' width={800} height={800} alt="illustration" priority />
+        <Image
+          src="/images/illustrations/illustration-hero.png"
+          width={800}
+          height={800}
+          alt="illustration"
+          priority
+        />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
