584217e6dae86f59135a825a806ca06905e9f3ac
taravannheng <heng.t.taravann@gmail.com>
2023-03-16 17:36:53 +1100

    refactor: remove work experience section

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from 'react';
 
 import HeroSection from '../components/hero/hero';
 import SkillsSection from '../components/skills/skills';
-import WorkSection from '../components/work/work';
 import ContactSection from '../components/contact/contact';
 import FooterSection from '../components/footer/footer';
 import Loader from '../components/loader/loader';
