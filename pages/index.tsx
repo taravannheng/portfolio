5d042da8fcb280672254cd80fb196451e1e1cb87
taravannheng <heng.t.taravann@gmail.com>
2023-03-16 17:27:22 +1100

    refactor: remove work experience section

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,6 @@ const HomePage = () => {
         <div>
           <HeroSection />
           <SkillsSection />
-          <WorkSection />
           <ContactSection />
           <FooterSection />
         </div>
