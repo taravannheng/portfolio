fb09689f051fdec8eebd5492d77e82ad4ec5bf44
Taravann Heng <heng.t.taravann@gmail.com>
2023-06-05 03:29:51 +1000

    refactor(style): change font to Inter

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet"></link>
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet"></link>
         </Head>
         <body>
           <Main />
