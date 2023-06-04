163940efb2976cc3e5bfaf18c2ba51dce552a24b
Taravann Heng <heng.t.taravann@gmail.com>
2023-06-05 05:09:58 +1000

    refactor(component): remove margin right from last tab

--- components/home/home.style.js
+++ components/home/home.style.js
@@ -15,13 +15,21 @@ export const TabSC = styled(Tab)`
   background-color: #262626 !important;
   color: #FFFFFF !important;
   margin: 0px !important;
-  padding: 8px 40px !important;
+  padding: 0px 40px !important;
   margin-right: 16px !important;
   text-transform: none !important;
   font-family: 'Inter', sans-serif !important;
   font-weight: 400 !important;
   font-size: 14px !important;
   transition: background-color 0.3s ease-in-out;
+
+  @media only screen and (max-width: 600px) {
+    padding: 0px 24px !important;
+  }
+
+  &:last-child {
+    margin-right: 0 !important;
+  }
 
   &:hover {
     background-color: #3076FF !important;
