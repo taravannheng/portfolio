d228dede5e363ed36a75ffd932410fa284384b2e
Taravann Heng <heng.t.taravann@gmail.com>
2023-06-05 22:16:45 +1000

    refactor(component): remove social links from footer

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -10,36 +10,6 @@ const getYear = () => {
 const FooterSection = () => {
   return (
     <footer className={classes.footer}>
-      <div className={classes.footer__social}>
-        <div className={classes.social__icon}>
-          <Link
-            href="https://www.linkedin.com/in/taravann-heng"
-          >
-            <a target="_blank">
-              <Image
-                src="/images/social/linkedin.svg"
-                width={36}
-                height={36}
-                alt="LinkedIn"
-                loading='eager'
-              />
-            </a>
-          </Link>
-        </div>
-        <div className={classes.social__icon}>
-          <Link href="https://github.com/taravannheng">
-            <a target="_blank">
-              <Image
-                src="/images/social/github.svg"
-                width={36}
-                height={36}
-                alt="GitHub"
-                loading='eager'
-              />
-            </a>
-          </Link>
-        </div>
-      </div>
       <div className={classes.footer__text}>
         <p className={classes.text__info}>
           Designed and Built by Taravann Heng
