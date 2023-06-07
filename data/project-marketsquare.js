346b4f4802e90ae6469b85d20320575ae0c260fb
Taravann Heng <heng.t.taravann@gmail.com>
2023-06-08 08:45:48 +1000

    refactor(data): update project data

--- data/project-marketsquare.js
+++ data/project-marketsquare.js
@@ -9,12 +9,12 @@ import {
 
 const marketsquare = {
   id: 'marketsquare',
-  imgUrl: "/images/projects/project-1.png",
+  imgUrl: "https://firebasestorage.googleapis.com/v0/b/personal-portfolio-d2496.appspot.com/o/marketsquare.png?alt=media&token=6758b2fd-8cf8-4d3f-8f94-a5cc1ae084a2&_gl=1*mdtatd*_ga*MTA3MDYzMzc5Ni4xNjg1OTYxNzE3*_ga_CW55HF8NVT*MTY4NjE3NjQ5My4zLjEuMTY4NjE3NjU5OS4wLjAuMA..",
   slides: [
     {
       id: 'marketsquare',
       type: "summary",
-      imgUrl: "/images/projects/project-1.png",
+      imgUrl: "https://firebasestorage.googleapis.com/v0/b/personal-portfolio-d2496.appspot.com/o/marketsquare-1.png?alt=media&token=057c9f27-6a59-4a6b-9163-c801645b489d&_gl=1*txbzla*_ga*MTA3MDYzMzc5Ni4xNjg1OTYxNzE3*_ga_CW55HF8NVT*MTY4NjE3NjQ5My4zLjEuMTY4NjE3NzU4NS4wLjAuMA..",
       title: "Marketsquare",
       description:
         `Marketsquare is an innovative ecommerce platform designed to provide users with a seamless shopping experience. With a wide range of product catalogs, shoppers can easily browse through an extensive collection of items, ranging from fashion and electronics to home decor and more. Each product listing provides comprehensive details, enabling customers to make informed decisions. From product descriptions and specifications to customer reviews, Marketsquare ensures that users have all the information they need to confidently choose the perfect product. \nWhen it's time to make a purchase, the checkout process is smooth and secure. Marketsquare integrates with Stripe, a leading online payment gateway, allowing customers to securely complete transactions using their preferred payment methods. With advanced encryption and fraud prevention measures, users can trust that their payment information is safe and protected. \nTo enhance the shopping experience, Marketsquare incorporates a user-friendly cart system. Customers can easily add items to their cart, review their selections, and make adjustments as needed. The cart provides a convenient overview of the chosen products, quantities, and prices. \nBefore finalizing a purchase, users can view an order summary that includes a breakdown of the total cost, including taxes and shipping fees. This ensures transparency and eliminates any surprises during the checkout process. \nMarketsquare strives to create a user-centric platform that prioritizes simplicity, security, and convenience. Whether you're a frequent online shopper or looking to explore new products, Marketsquare offers a comprehensive solution for all your ecommerce needs.`,
@@ -22,7 +22,7 @@ const marketsquare = {
     },
     {
       type: "functionality",
-      imgUrl: "/images/projects/project-2.png",
+      imgUrl: "https://firebasestorage.googleapis.com/v0/b/personal-portfolio-d2496.appspot.com/o/marketsquare-2.png?alt=media&token=b9172a53-7b08-4f9f-bcc9-927b0066790b&_gl=1*bck5iq*_ga*MTA3MDYzMzc5Ni4xNjg1OTYxNzE3*_ga_CW55HF8NVT*MTY4NjE3NjQ5My4zLjEuMTY4NjE3NzYyNi4wLjAuMA..",
       title: "Functionalities",
       items: [
         {
@@ -53,7 +53,7 @@ const marketsquare = {
     },
     {
       type: "technology",
-      imgUrl: "/images/projects/project-3.png",
+      imgUrl: "https://firebasestorage.googleapis.com/v0/b/personal-portfolio-d2496.appspot.com/o/marketsquare-3.png?alt=media&token=1243057b-aec8-4bb3-ae13-e5c59651dade&_gl=1*1khkyoa*_ga*MTA3MDYzMzc5Ni4xNjg1OTYxNzE3*_ga_CW55HF8NVT*MTY4NjE3NjQ5My4zLjEuMTY4NjE3NzY0My4wLjAuMA..",
       title: "Frontend Technologies",
       items: [
         {
@@ -69,57 +69,61 @@ const marketsquare = {
           text: "Redux",
         },
         {
-          icon: "/images/languages/tailwind.svg",
-          text: "TailwindCSS",
-        },
-        {
           icon: "/images/languages/materialui.svg",
           text: "MaterialUI",
-        },
-        {
-          icon: "/images/languages/sass.svg",
-          text: "Sass",
         },
       ],
     },
     {
       type: "technology",
-      imgUrl: "/images/projects/project-4.png",
+      imgUrl: "https://firebasestorage.googleapis.com/v0/b/personal-portfolio-d2496.appspot.com/o/marketsquare-4.png?alt=media&token=41ec06d9-43bd-4bc8-ada1-8e514675d981&_gl=1*1bjmy5j*_ga*MTA3MDYzMzc5Ni4xNjg1OTYxNzE3*_ga_CW55HF8NVT*MTY4NjE3NjQ5My4zLjEuMTY4NjE3NzY1NC4wLjAuMA..",
       title: "Backend Technologies",
       items: [
         {
-          icon: "/images/languages/nextjs.svg",
-          text: "NextJS",
+          icon: "/images/languages/nodejs.svg",
+          text: "NodeJS",
         },
         {
-          icon: "/images/languages/python.svg",
-          text: "Python",
+          icon: "/images/languages/expressjs.svg",
+          text: "Express",
         },
         {
-          icon: "/images/languages/aws.svg",
-          text: "AWS",
+          icon: "/images/languages/mongodb.svg",
+          text: "MongoDB",
+        },
+        {
+          icon: "/images/languages/firebase.svg",
+          text: "Firebase",
+        },
+        {
+          icon: "/images/languages/stripe.svg",
+          text: "Stripe",
+        },
+        {
+          icon: "/images/languages/lambda.svg",
+          text: "AWS Lambda",
         },
       ],
     },
     {
       type: "socials",
-      imgUrl: "/images/projects/project-5.png",
+      imgUrl: "https://firebasestorage.googleapis.com/v0/b/personal-portfolio-d2496.appspot.com/o/marketsquare-5.png?alt=media&token=9e125592-e91d-4279-9712-f4bec892fa2e&_gl=1*1f68hkb*_ga*MTA3MDYzMzc5Ni4xNjg1OTYxNzE3*_ga_CW55HF8NVT*MTY4NjE3NjQ5My4zLjEuMTY4NjE3NzY2Ny4wLjAuMA..",
       title: "Check It Out 👇",
       items: [
         {
           icon: "/images/social/new-window.svg",
           text: "Demo",
-          href: "https://www.google.com"
+          href: "https://marketsquare.vercel.app"
         },
         {
           icon: "/images/social/github.svg",
           text: "GitHub",
-          href: "https://www.google.com"
+          href: "https://github.com/taravannheng/marketsquare"
         },
         {
           icon: "/images/social/dribbble.svg",
           text: "Dribbble",
-          href: "https://www.google.com"
+          href: "https://dribbble.com/shots/21591624-Marketsquare"
         },
       ],
     },
